b8292617cb083d86039124143a94dc7ff2965ae8
Eugen Ciur <eugenci@yandex.ru>
2013-09-13 17:39:26 +0200

    refactoring of posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,8 +25,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
-    @post.user = current_user
+    @post = current_user.posts.build(post_params)
 
     respond_to do |format|
       if @post.save
