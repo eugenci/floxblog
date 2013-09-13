eaf17dcdce40e1fb5f905215e2a56038d43d375f
Eugen Ciur <eugenci@yandex.ru>
2013-09-13 18:06:37 +0200

    Refactoring seeds. Update readme

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create! email: 'user@foxblog.com',
-                    password: 'test1234',
-                    first_name: 'User',
-                    last_name: 'Userberg',
-                    username: 'berg'
-user.save!
+User.create! email: 'user@foxblog.com',
+             password: 'test1234',
+             first_name: 'User',
+             last_name: 'Userberg',
+             username: 'berg'
