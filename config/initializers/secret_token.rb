efeae7f8cf27af4e9a0c4a7e4435402aff38a8f9
Eugen Ciur <eugenci@yandex.ru>
2013-09-13 14:35:55 +0200

    Scafflod for basic application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foxblog::Application.config.secret_key_base = '0f637839ecf7871d03a33c22ed95ac3de801d6d62d4337344cf4ba9ab7fab6fb567b43ca2a212ed493c8676bc3809b1c56ceb59eccfa58f83c1a69a53bc981f1'
